b3e761dec74b130e75c1e86d10b1c5e3f7d42f4e
GrayUnit <grayunit@outlook.fr>
2021-01-07 14:13:10 +0100

    Utilisation du décorateur @output

--- src/app/order/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/order/pages/page-list-orders/page-list-orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StateOrder } from 'src/app/shared/enums/state-order.enum';
+import { BtnI } from 'src/app/shared/interfaces/btn-i';
 import { Order } from 'src/app/shared/models/order';
 import { OrdersService } from '../../services/orders.service';
 
@@ -13,6 +14,9 @@ export class PageListOrdersComponent implements OnInit {
   public collection: Order[];
   public headers: string[];
   public states = Object.values(StateOrder);
+  public btnRoute: BtnI;
+  public btnHref: BtnI;
+  public btnAction: BtnI;
 
   constructor(private orderService: OrdersService) { }
 
@@ -29,6 +33,18 @@ export class PageListOrdersComponent implements OnInit {
       'Total TTC',
       'State'
     ];
+    this.btnRoute = {
+      label: 'Add an order',
+      route: 'add'
+    };
+    this.btnHref = {
+      label: 'Go to Google',
+      href: 'http://www.google.fr'
+    };
+    this.btnAction = {
+      label: 'Open dialogue',
+      action: true
+    };
   }
 
   public changeState(item: Order, event) {
@@ -36,6 +52,10 @@ export class PageListOrdersComponent implements OnInit {
       // traiter la res de l'api, codes erreur etc...
       item.state = res.state;
     });
+  }
+
+  public openPopUp() {
+    console.log('open popup');
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { BtnComponent } from './components/btn/btn.component';
 import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
 import { TableDarkComponent } from './components/table-dark/table-dark.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -22,6 +23,7 @@ import { TableDarkComponent } from './components/table-dark/table-dark.component
     TotalPipe,
     StateDirective,
     TableDarkComponent,
+    RouterModule,
   ]
 })
 export class SharedModule { }
