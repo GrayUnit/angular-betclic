d455bb50b4360811a176e52867dd465155fb3948
GrayUnit <grayunit@outlook.fr>
2021-01-07 11:26:30 +0100

    Router partie 1: router-outlet et directive routerLink

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
+import { PageListClientsComponent } from './pages/page-list-clients/page-list-clients.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageListClientsComponent],
   imports: [
     CommonModule,
     ClientRoutingModule
--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrderRoutingModule } from './order-routing.module';
+import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageListOrdersComponent],
   imports: [
     CommonModule,
     OrderRoutingModule
